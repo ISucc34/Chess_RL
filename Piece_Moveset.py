9bf2cee2dcfaa8b150f0fd14f7b2b2223eda8844
Isaac Kristopher Gonzalez <142045835+ISucc34@users.noreply.github.com>
2026-04-02 11:09:02 -0500

    update readme and  abstract class

--- Piece_Moveset.py
+++ Piece_Moveset.py
@@ -3,16 +3,34 @@ import pygame
 
 #Abstract Class template
 class Piece():
-    def __init__(self):
-        pass
+    def __init__(self, currPos):
+        self.currPos = currPos
+        self.value = 0
+    def update(self, newPos):
+        self.currPos = newPos
     def getSprite(self):
+        self.rect = (0,0,64,64)
         pass
 
+class Pawn():
+    def __init__(self, pos):
+        self.pos = pos
+        self.value = 9
+    def getSprite(self):
+        self.rect = (0,0,64,64)
+        self.sprite = "t.png"
+        return self.rect, self.sprite
+        
+    def spawnSprite(self, window):
+        self.window = window
+        self.window.blit(self.sprite, (20,20), self.rect)
+
+    def move(pos):
+        pos
 
 class Rook():
-    def __init__(self, currPos, newPos):
+    def __init__(self, currPos):
         self.currPos = currPos
-        self.newPos = newPos
         self.value = 9
     def move(currPos, newPos):
         pass
@@ -31,21 +49,7 @@ class Knight():
     def move(pos):
         pos
 
-class Pawn():
-    def __init__(self, pos):
-        self.pos = pos
-        self.value = 9
-    def getSprite(self):
-        self.rect = (0,0,64,64)
-        self.sprite = "t.png"
-        return self.rect, self.sprite
-        
-    def spawnSprite(self, window):
-        self.window = window
-        self.window.blit(self.sprite, (20,20), self.rect)
 
-    def move(pos):
-        pos
 
 class Queen():
     def __init__(self, pos):
--- README.md
+++ README.md
@@ -1,10 +1,35 @@
 # Chess Reinforcement Learning
 
 ## Overview
-This project is a reinforcment learning project that uses chess as the environment
+This project is a reinforcment learning project that uses chess as the environment. The end goal is to get a working model that can at minimum play chess. 
 
 ## Install
 
-## Technologies
+### Set up environment
+
+Through Conda
+
+```
+conda create env -n <your_env_name> python==3.13.5
+
+```
+
+Through -venv
+
+```
+
+```
+
+### Install necessary libraries
+
+```
+
+```
+
+## Technolgies/Libraries
+
+Pygame
 
 ## Process
+
+I first began with creating the game of chess.
